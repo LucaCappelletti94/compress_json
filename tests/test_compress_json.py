ef83050df463fdff04345e32f0a4d6dc0230208a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-04-04 12:02:49 +0200

    Resolved code smell

--- tests/test_compress_json.py
+++ tests/test_compress_json.py
@@ -1,5 +1,4 @@
 import compress_json
-import os
 import shutil
 from random_dict import random_dict
 from dict_hash import sha256
@@ -13,12 +12,12 @@ def test_compress_json():
         path = f"random_dirs/test.json.{ext}"
         compress_json.dump(D, path)
         assert key == sha256(compress_json.load(path))
-    
+
     shutil.rmtree("random_dirs")
 
     for ext in extensions:
         path = f"random_dirs/test.json.{ext}"
         compress_json.local_dump(D, path)
         assert key == sha256(compress_json.local_load(path))
-    
+
     shutil.rmtree("tests/random_dirs")
