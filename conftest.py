d69059fec72f131a0fb296622e3e2f188aef0a0a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-07-24 15:22:01 +0200

    Extended pytest setup for github CI

--- conftest.py
+++ conftest.py
@@ -1,4 +1,10 @@
 """pytest configuration file."""
+
+import os
 from pytest_readme import setup
 
 setup()
+
+# We move the file test_readme.py from the root directory
+# to the tests directory.
+os.rename("test_readme.py", "tests/test_readme.py")
